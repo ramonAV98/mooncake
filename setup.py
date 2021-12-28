80298deecb444d0840f9460d6ba82109a12e72a0
Ramón Amézquita <ramon.amez@kuona.co>
2021-12-28 16:30:03 -0700

    setup file

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+   name='mooncake',
+   version='1.0',
+   description='Ai system',
+   author='chookity',
+   packages=['mooncake'],  # same as name
+   install_requires=[
+      'numpy==1.21.0',
+      'pandas==1.2.4',
+      'scikit-learn==0.24.1',
+      'skorch==0.10.0',
+      'torch>=1.5.0',
+      'pytorch-forecasting==0.9.0'
+    ]
+)

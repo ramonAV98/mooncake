eceaed2ac700f72f2ea6c303dd45f46448f44149
Ramón Amézquita <ramon.amez@kuona.co>
2022-01-03 11:39:09 -0700

    changed deps version to greater or equal

--- setup.py
+++ setup.py
@@ -7,11 +7,11 @@ setup(
    author='chookity',
    packages=['mooncake'],  # same as name
    install_requires=[
-      'numpy==1.21.0',
-      'pandas==1.2.4',
+      'numpy>=1.21.0',
+      'pandas>=1.2.4',
       'scikit-learn==0.24.1',
-      'skorch==0.10.0',
+      'skorch>=0.10.0',
       'torch>=1.5.0',
-      'pytorch-forecasting==0.9.0'
+      'pytorch-forecasting>=0.9.0'
     ]
 )

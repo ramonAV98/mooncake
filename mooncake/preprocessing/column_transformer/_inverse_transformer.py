57bc27efa9c200946c5278dc9c11ab4e369c68c1
Ramón Amézquita <ramon.amez@kuona.co>
2022-04-17 00:01:33 -0600

    Return None when `columns_to_inv` is empty`

--- mooncake/preprocessing/column_transformer/_inverse_transformer.py
+++ mooncake/preprocessing/column_transformer/_inverse_transformer.py
@@ -31,32 +31,44 @@ class ColumnTransformerInverseTransformer:
         self._non_invertible_columns = []
         inv_transforms = []
 
-        # Notice each transformer only inv transforms the columns it
-        # originally fitted.
         for triplet in self.column_transformer.transformers_:
             name, transformer, features = triplet
 
-            # Obtain columns to inverse transform.
+            # Notice each transformer only inv transforms the columns it
+            # originally fitted.
             columns_to_inv = self.components_getter.get_columns_by_name(name)
-
-            # Inverse transform.
-            columns_inverse_transformed = self._inverse_transform_columns(
+            inverse_transformed_columns = self._inverse_transform_columns(
                 X, transformer, columns_to_inv)
 
             # Check for non invertible columns.
-            if columns_inverse_transformed is None:
-                non_invertible = self.components_getter.get_columns_by_name(
-                    name, transformed=False)
-                self._non_invertible_columns.extend(non_invertible)
+            if inverse_transformed_columns is None:
+                self._save_non_invertible(name)
             else:
-                inv_transforms.append(columns_inverse_transformed)
+                inv_transforms.append(inverse_transformed_columns)
 
-        # Stack inverse transformations of all columns (axis=1).
-        X_inv = np.hstack(inv_transforms)
-        return X_inv
+        return np.hstack(inv_transforms)
 
     def get_non_invertible_columns(self):
+        """Returns non invertible columns.
+
+        Returns
+        -------
+        non_invertible_columns : list
+        """
         return self._non_invertible_columns
+
+    def _save_non_invertible(self, name=None, non_invertible=None):
+        """Saves non invertible columns in private attribute
+        `_non_invertible_columns`.
+        """
+        if non_invertible is None:
+            non_invertible = []
+
+        if name is not None:
+            non_invertible = self.components_getter.get_columns_by_name(
+                name, transformed=False)
+
+        self._non_invertible_columns.extend(non_invertible)
 
     def _inverse_transform_columns(self, X, transformer, columns_to_inv):
         """Performs an inverse transformation on the given `columns_to_inv`
@@ -81,6 +93,12 @@ class ColumnTransformerInverseTransformer:
             Array with inverse transformed columns. An empty array is returned
             when inverse transformation is not possible.
         """
+        # Similar to the :class:`sklearn.compose.ColumnTransformer`
+        # :meth:`transform` method, the inverse transform should also
+        # ignore whenever the given columns are empty.
+        if not columns_to_inv:
+            return None
+
         missing_columns = [c for c in columns_to_inv if c not in X]
 
         if hasattr(transformer, 'inverse_transform'):
@@ -89,11 +107,11 @@ class ColumnTransformerInverseTransformer:
             return transformer.inverse_transform(X[columns_to_inv])
 
         if transformer == 'passthrough':
-            # For this case, the inverse transformation can proceed even with
-            # missing columns. However, they are added to the non invertible
-            # list.
+            # For the `transformer == "passthrough"` case, the inverse
+            # transformation continues even with missing columns. However,
+            # they are added to the non invertible list.
             if missing_columns:
-                self._non_invertible_columns.extend(missing_columns)
+                self._save_non_invertible(non_invertible=missing_columns)
             columns_to_inv = [c for c in columns_to_inv if c in X]
             return X[columns_to_inv].values
         return None
